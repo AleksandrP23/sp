8af5e86bf00b3643facc19537da22c61b436396c
AleksandrP23 <30500631+AleksandrP23@users.noreply.github.com>
2022-06-27 18:56:57 +0300

    Update script.js

--- docs/js/script.js
+++ docs/js/script.js
@@ -283,45 +283,21 @@ function canvas(animation) {
 }
 
 
+var container = document.getElementById('container');
+var rows = container.children;
 
-const wrapper = document.querySelector('.index-server__list');
-const empty = Array.from(document.querySelectorAll('.oc'));
-const drag = document.querySelector('.index-server__item');
-
-drag.addEventListener('touchmove', touchMove);
-drag.addEventListener('touchend', touchEnd);
-
-let itemAppend = null;
-// ------------------------ touchMove
-function touchMove(event) {
-  event.preventDefault();
-
-  let touch = event.targetTouches[0];
-  drag.style.top = `${touch.pageY - (wrapper.offsetTop) - (drag.offsetWidth / 2)}px`;
-  drag.style.left = `${touch.pageX - (wrapper.offsetLeft) - (drag.offsetHeight / 2)}px`;
-
-  empty.map(item => {
-    if (
-      drag.getBoundingClientRect().top + drag.offsetWidth / 2 < item.getBoundingClientRect().bottom &&
-      drag.getBoundingClientRect().right - drag.offsetWidth / 2 > item.getBoundingClientRect().left &&
-      drag.getBoundingClientRect().bottom - drag.offsetWidth / 2 > item.getBoundingClientRect().top &&
-      drag.getBoundingClientRect().left + drag.offsetWidth / 2 < item.getBoundingClientRect().right
-    ) {
-      item.classList.add('active');
-      itemAppend = item;
-    } else {
-      item.classList.remove('active');
-    }
-  });
-}
-
-function touchEnd() {
-  if (itemAppend.classList.contains('active')) {
-    itemAppend.append(this);
-    this.style.top = `${itemAppend.offsetTop}px`;
-    this.style.left = `${itemAppend.offsetLeft}px`;
-  } else {
-    this.style.top = `${itemAppend.offsetTop}px`;
-    this.style.left = `${itemAppend.offsetLeft}px`;
+// forEach method from https://toddmotto.com/ditch-the-array-foreach-call-nodelist-hack/
+var nodeListForEach = function (array, callback, scope) {
+  for (var i = 0; i < array.length; i++) {
+		callback.call(scope, i, array[i]);
   }
-}
+};
+
+var sortableTable = dragula([container]);
+
+sortableTable.on('dragend', function() {
+  nodeListForEach(rows, function (index, row) {
+    row.lastElementChild.textContent = index + 1;
+    row.dataset.rowPosition = index + 1;
+  });
+});
